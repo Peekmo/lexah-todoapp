f951957f9652e01a6f6816f233c23589fe9dbc59
Peekmo <axel.anceau@gmail.com>
2015-10-18 17:59:58 +0200

    Create modules

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 var Main = function() { };
 Main.main = function() {
 	var mod = window.angular.module("todoapp",[]).controller("TodoController",["$scope",function(scope) {
-		return new TodoController(scope);
+		return new controller_TodoController(scope);
 	}]);
-};
-var TodoController = function(scope) {
-	scope.data = "Hello";
 };
 var angular__$Angular_TranscludeFn_$Impl_$ = {};
 angular__$Angular_TranscludeFn_$Impl_$.call = function(this1,scope,link) {
@@ -17,6 +14,9 @@ angular__$Angular_TranscludeFn_$Impl_$.withoutScope = function(this1,link) {
 };
 var angular_internal_ModuleMacros = function() { };
 var angular_internal_InjectorMacros = function() { };
+var controller_TodoController = function(scope) {
+	scope.data = "Hello";
+};
 var q = window.jQuery;
 var js = js || {}
 js.JQuery = q;
