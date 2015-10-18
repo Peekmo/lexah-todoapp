98e6321e49e4ddfbd656b0cb75f6612e6ea1c73d
Peekmo <axel.anceau@gmail.com>
2015-10-18 20:53:00 +0200

    Deletion

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 (function (console) { "use strict";
 var HxOverrides = function() { };
+HxOverrides.indexOf = function(a,obj,i) {
+	var len = a.length;
+	if(i < 0) {
+		i += len;
+		if(i < 0) i = 0;
+	}
+	while(i < len) {
+		if(a[i] === obj) return i;
+		i++;
+	}
+	return -1;
+};
+HxOverrides.remove = function(a,obj) {
+	var i = HxOverrides.indexOf(a,obj,0);
+	if(i == -1) return false;
+	a.splice(i,1);
+	return true;
+};
 HxOverrides.iter = function(a) {
 	return { cur : 0, arr : a, hasNext : function() {
 		return this.cur < this.arr.length;
@@ -38,6 +56,16 @@ controller_TodoController.prototype = {
 	create: function(value) {
 		if(value != "" && value != null) this.todos.push(new model_Todo(value));
 	}
+	,'delete': function(id) {
+		var $it0 = HxOverrides.iter(this.todos);
+		while( $it0.hasNext() ) {
+			var todo = $it0.next();
+			if(todo.id == id) {
+				HxOverrides.remove(this.todos,todo);
+				break;
+			}
+		}
+	}
 	,init_app: function() {
 		var _g = this;
 		this.todos = [];
@@ -50,6 +78,9 @@ controller_TodoController.prototype = {
 		this.scope.create = function() {
 			_g.create(Reflect.field(_g.scope,"new_todo"));
 		};
+		this.scope["delete"] = function(id) {
+			_g["delete"](id);
+		};
 	}
 };
 var model_Todo = function(value) {
@@ -59,6 +90,9 @@ var model_Todo = function(value) {
 function $iterator(o) { if( o instanceof Array ) return function() { return HxOverrides.iter(o); }; return typeof(o.iterator) == 'function' ? $bind(o,o.iterator) : o.iterator; }
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+if(Array.prototype.indexOf) HxOverrides.indexOf = function(a,o,i) {
+	return Array.prototype.indexOf.call(a,o,i);
+};
 var q = window.jQuery;
 var js = js || {}
 js.JQuery = q;
